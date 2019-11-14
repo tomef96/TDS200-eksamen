08bf2de95508bd90f51a49ef8872254878412b95
Tom <tomef96@gmail.com>
2019-11-14 22:47:59 +0100

    init, login, register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,43 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+const firebaseConfig = {
+    apiKey: 'AIzaSyCxsTfXYqgdTTJBBaU_f8mRNpDh4xKD-C4',
+    authDomain: 'tds200-h19-5002.firebaseapp.com',
+    databaseURL: 'https://tds200-h19-5002.firebaseio.com',
+    projectId: 'tds200-h19-5002',
+    storageBucket: 'tds200-h19-5002.appspot.com',
+    messagingSenderId: '154919526592',
+    appId: '1:154919526592:web:5aa39be001fb17e02ecd25'
+};
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireStorageModule,
+        AngularFireAuthGuardModule
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
-  constructor() {}
-
+    constructor(private db: AngularFirestore) {
+        const test = db.collection('test').valueChanges();
+        test.subscribe(console.log);
+    }
 }
